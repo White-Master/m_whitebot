b8a957d7f039326396af579291ea93b370c0f547
Ramiro Bou <bouramiro@gmail.com>
2013-10-09 00:09:50 -0300

    WTF?!

--- m_whitebot.php
+++ m_whitebot.php
@@ -50,36 +50,4 @@ class key{
         $core->irc->unregisterTimeid($this->timehandlerid);
     }
 
-
-}
-                break;
-                case "new":
-                    $s="03".$fg->query->recentchanges[0]->user." ha 03creado el articulo [[".$fg->query->recentchanges[0]->title."]] con el siguiente comentario: 07".$fg->query->recentchanges[0]->comment;
-                    break;
-                case "log":
-					switch ($fg->query->recentchanges[0]->logtype){
-						case "rights":
-							if($fg->query->recentchanges[0]->rights->old==""){$old="(ninguno)";}else{$old=$fg->query->recentchanges[0]->rights->old;}
-							if($fg->query->recentchanges[0]->rights->new==""){$new="(ninguno)";}else{$new=$fg->query->recentchanges[0]->rights->new;}
-							$s="Se han cambiado los privilegios de [[".$fg->query->recentchanges[0]->title."]] de ".$old." a ".$new;
-							break;
-						 case "block":
-							$s="\00303{$fg->query->recentchanges[0]->user}\003 ha \00305bloqueado\003 a \002[[{$fg->query->recentchanges[0]->title}]]\002. Duración: \002{$fg->query->recentchanges[0]->block->duration}\002. Razón: \00307{$fg->query->recentchanges[0]->comment}";
-							break;
-						 case "delete":
-							$s = "\00303{$fg->query->recentchanges[0]->user}\003 ha \00304borrado\003 \002[[{$fg->query->recentchanges[0]->title}]]\002: \00307{$fg->query->recentchanges[0]->comment}\003";
-							break;
-						
-					}
-            }
-            $irc->send("PRIVMSG #cobot :$s");
-        }
-
-    }
-    
-    public function __destruct(){
-        $core->irc->unregisterTimeid($this->timehandlerid);
-    }
-
-
 }
