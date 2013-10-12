a8f14efeb195186d57dc668bf74a88bae0fe9ce1
Ramiro Bou <bouramiro@gmail.com>
2013-10-12 00:16:40 -0300

    Añadiendo link al diff en los cambios recientes.

--- m_whitebot.php
+++ m_whitebot.php
@@ -55,9 +55,8 @@ class key{
             switch($fg->query->recentchanges[0]->type){
                 case "edit":
                     $s="03".$fg->query->recentchanges[0]->user." ha editado el articulo \00310[[".$fg->query->recentchanges[0]->title."]]\003 con el siguiente comentario: 07".$fg->query->recentchanges[0]->comment;
-                    if(@isset($fg->query->recentchanges[0]->minor)){ $s.="11 Esta es una edición menor."; }elseif($fg->query->recentchanges[0]->title=="Wikiviajes:Bugs"){
-						$s.=" \00311http://es.wikivoyage.org/w/index.php?title=Wikiviajes:Bugs&diff";
-					}
+                    if(@isset($fg->query->recentchanges[0]->minor)){ $s.="11 Esta es una edición menor."; }
+						$s.=" \00311http://es.wikivoyage.org/w/index.php?title=".urlencode(str_replace(" ","_", $fg->query->recentchanges[0]->title))."&diff";
                     break;
                 case "new":
                     $s="03".$fg->query->recentchanges[0]->user." ha creado el articulo \00310[[".$fg->query->recentchanges[0]->title."]]\003 con el siguiente comentario: 07".$fg->query->recentchanges[0]->comment;
